6db0a16f924ce77b6573018fb41cad12d61b00e6
Parker <pgrant2@ncsu.edu>
2017-11-16 01:06:33 -0500

    Moved version 2 to main version
    Focusing on my current bot in main directory.  It's the only one I'm going to make decent progress with.

--- MyBot.java
+++ MyBot.java
@@ -1,8 +1,6 @@
 import hlt.*;
 
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public class MyBot {
 
@@ -27,41 +25,77 @@ public class MyBot {
         if (ship.getDockingStatus() != Ship.DockingStatus.Undocked) {
           continue;
         }
-
+        int me = ship.getOwner();
         int i = 0;
         int count = 0;
         for (final Planet planet : gameMap.getAllPlanets().values()) {
-          count++;
+          if(planet != null)
+            count++;
         }
         Planet[] nearbyPlanets = new Planet[count];
         double[] distance = new double[count];
         for (final Planet planet : gameMap.getAllPlanets().values()) {
-          nearbyPlanets[i] = gameMap.getPlanet(i);
-          distance[i] = planet.getDistanceTo(ship);
-          i++;
+          if(planet != null) {
+            nearbyPlanets[i] = gameMap.getPlanet(i);
+            distance[i] = planet.getDistanceTo(ship);
+            i++;
+          }
         }
 
         //sorting planet array by distance
-
-
-        for (final Planet planet : gameMap.getAllPlanets().values()) {
-          if (planet.isOwned()) {
-            continue;
-          }
-
-          if (ship.canDock(planet)) {
-            moveList.add(new DockMove(ship, planet));
-            break;
-          }
-
-          final ThrustMove newThrustMove = Navigation.navigateShipToDock(gameMap, ship, planet, Constants.MAX_SPEED);
-            if (newThrustMove != null) {
-              moveList.add(newThrustMove);
-            }
-
-            break;
+        for(i = 1; i < count; i++) {
+          if(nearbyPlanets[i] != null) {
+            double tmp = distance[i];
+            Planet tmpPlanet = nearbyPlanets[i];
+            int j = i - 1;
+              while(j >= 0 && distance[j] > tmp) {
+                nearbyPlanets[j+1] = nearbyPlanets[j];
+                distance[j+1] = distance[j];
+                j--;
+              }
+            nearbyPlanets[j+1] = tmpPlanet;
+            distance[j+1] = tmp;
           }
         }
+
+        for (i = 0; i < count; i++) {
+          if(nearbyPlanets[i] != null) {
+            if (nearbyPlanets[i].isOwned()) {
+              if(nearbyPlanets[i].getOwner() != me || nearbyPlanets[i].isFull()) {
+                //attack docked ships
+                if(nearbyPlanets[i].getOwner() != me) {
+                  int ownerOfShip = nearbyPlanets[i].getOwner();
+                  List<Integer> dockedShips = nearbyPlanets[i].getDockedShips();
+                  for(final int shipId : dockedShips) {
+                    Ship enemy = gameMap.getShip(ownerOfShip, shipId);
+                    final ThrustMove newThrustMove = Navigation.navigateShipTowardsTarget(gameMap, ship, enemy, Constants.MAX_SPEED, true, Constants.MAX_NAVIGATION_CORRECTIONS, Math.PI/180.0 );
+                      if (newThrustMove != null) {
+                        moveList.add(newThrustMove);
+                      }
+                      break;
+                  }
+                  break;
+                }
+                else
+                  continue;
+              }
+            }
+
+            if (ship.canDock(nearbyPlanets[i])) {
+              moveList.add(new DockMove(ship, nearbyPlanets[i]));
+              break;
+            }
+
+            final ThrustMove newThrustMove = Navigation.navigateShipToDock(gameMap, ship, nearbyPlanets[i], Constants.MAX_SPEED);
+              if (newThrustMove != null) {
+                moveList.add(newThrustMove);
+              }
+              break;
+          }
+        }
+      }
+
+
         Networking.sendMoves(moveList);
     }
   }
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # myBot
+
+Bot created for Two Sigma's Halite 2 programming challenge.
+Originally downloaded source code to my computer, so comments are around my added blocks.
